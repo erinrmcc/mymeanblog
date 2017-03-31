67dd6b6344db46459e48f86add74f6c6b71ca82c
Erin McCaffrey <erinmccaffrey@Erins-MacBook-Pro.local>
2017-03-30 20:04:24 -0400

    added the login authentication route that issues a jwt if the user provides email and password. Additionally, we fixed the user model to create hases of strings instead of shenanigans.

--- config/passport.js
+++ config/passport.js
@@ -3,4 +3,24 @@ const passport = require('passport'); //this is the module, not the file
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user.model');
 
-passport.use(new LocalStrategy(), function howWeAuth(username, password, done){})
+passport.use(new LocalStrategy({
+  usernameField: 'email'
+},
+  function howWeAuth(username, password, done){
+    User.findOne({email: username}, function(err, user){
+      if(err){
+        return done(err);
+      }
+      if(!user){ //if there is not a user with that email
+        return done(null, false, {
+          msg: "User not found"
+        });
+      }
+      if(!user.validPassword(password)){
+        return done(null, false, {
+          msg: "Authentication failed"
+        })
+      }
+      return done(null, user);
+    });
+  }));
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../models/user.model');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user.model');
+const passport = require('passport');
 
 router.post('/signup', function(req, res){
   var user = new User(req.body);
+  user.setPassword(req.body.password);
   user.save(function(err){
     if(err){
       res.status(500).json({
@@ -16,10 +18,28 @@ router.post('/signup', function(req, res){
     }
   });
 });
-router.post('/login', function(req, res){
-  res.status(200).json({
-    msg: 'logging in'
-  });
+router.post('/login', function(req, res, next){
+  passport.authenticate('local', function(err, user, data){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    if(!user){
+      return res.status(404).json({
+        msg: 'The username and/or password you have provided is incorrect'
+      });
+    }
+    console.log(user);
+    if(user && !user.validPassword(req.body.password)){
+      return res.status(401).json({
+        msg: 'The username and/or password you have provided is incorrect'
+      });
+    }
+    return res.status(200).json({
+      token: user.generateJwt()
+    });
+  })(req, res, next);
 });
 router.get('/users', function(req, res){
   User.find({}, function(err, users){
